68d0b59814d2d3d8708824d4a7cea9c8e73728b6
geesepixie <dayd08007@gmail.com>
2026-02-14 18:49:21 +0200

    Comments added C_K_th_Not_Divisible_by_n.cpp

--- Round_640_(Div. 4)/C_K_th_Not_Divisible_by_n.cpp
+++ Round_640_(Div. 4)/C_K_th_Not_Divisible_by_n.cpp
@@ -4,17 +4,20 @@ using namespace std ;
 void solve()
 {
     long long n , k ;
-    scanf( "%lld%lld" , &n , &k ) ;
+    scanf( "%lld%lld" , &n , &k ) ;          //  Read n and k for this test case
     
+    //  Calculate the k-th positive integer not divisible by n
+    //  Every block of (n - 1) valid numbers skips one multiple of n
     long long ans = k + ( k - 1 ) / ( n - 1 ) ;
-    printf( "%lld\n" , ans ) ;
+    
+    printf( "%lld\n" , ans ) ;                //  Print the result
 }
 
 int main()
 {
     int t ;
-    scanf( "%d" , &t ) ;
-    while ( t-- ) { solve() ; }
+    scanf( "%d" , &t ) ;                      //  Number of test cases
+    while ( t-- ) { solve() ; }               //  Solve each test case
         
     return 0 ;
 }
